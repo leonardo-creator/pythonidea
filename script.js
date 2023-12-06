fda3590c5c097c8207516c1e5fb3e383c99b6256
leonardo-creator <68303803+leonardo-creator@users.noreply.github.com>
2023-12-06 18:13:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageInput = document.getElementById("imageInput");
     const metadataList = document.getElementById("metadataList");
+    const processButton = document.getElementById("processButton");
+
+    let imageMetadataList = [];
 
     imageInput.addEventListener("change", function (event) {
-        const file = event.target.files[0];
+        const files = event.target.files;
 
-        if (file) {
+        if (files.length > 0) {
             clearMetadataList();
+            imageMetadataList = [];
 
-            const metadata = {
-                "File Name": file.name,
-                "File Size": `${(file.size / 1024).toFixed(2)} KB`,
-                "File Type": file.type,
-                "Last Modified": file.lastModifiedDate.toLocaleDateString(),
-            };
-
-            readImageMetadata(file, metadata);
+            Array.from(files).forEach((file, index) => {
+                readImageMetadata(file, index);
+            });
         }
+    });
+
+    processButton.addEventListener("click", function () {
+        const jsonMetadata = JSON.stringify(imageMetadataList, null, 2);
+        console.log(jsonMetadata);
+        alert("Verifique o console para ver o JSON gerado.");
     });
 
     function clearMetadataList() {
@@ -24,15 +29,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function displayMetadata(metadata) {
-        for (const key in metadata) {
-            const listItem = document.createElement("li");
-            listItem.className = "metadata-item";
-            listItem.innerHTML = `<strong>${key}:</strong> ${metadata[key]}`;
-            metadataList.appendChild(listItem);
-        }
+        const listItem = document.createElement("li");
+        listItem.className = "metadata-item";
+        listItem.innerHTML = `<strong>${metadata.name}:</strong> ${JSON.stringify(metadata, null, 2)}`;
+        metadataList.appendChild(listItem);
     }
 
-    function readImageMetadata(file, metadata) {
+    function readImageMetadata(file, index) {
         const reader = new FileReader();
 
         reader.onload = function (e) {
@@ -44,11 +47,16 @@ document.addEventListener("DOMContentLoaded", function () {
                     const lat = EXIF.getTag(this, "GPSLatitude");
                     const lon = EXIF.getTag(this, "GPSLongitude");
 
-                    if (lat && lon) {
-                        metadata["Latitude"] = convertDMSToDD(lat);
-                        metadata["Longitude"] = convertDMSToDD(lon);
-                    }
+                    const metadata = {
+                        name: file.name,
+                        "File Size": `${(file.size / 1024).toFixed(2)} KB`,
+                        "File Type": file.type,
+                        "Last Modified": file.lastModifiedDate.toLocaleDateString(),
+                        Latitude: lat ? convertDMSToDD(lat) : "N/A",
+                        Longitude: lon ? convertDMSToDD(lon) : "N/A"
+                    };
 
+                    imageMetadataList[index] = metadata;
                     displayMetadata(metadata);
                 });
             };
