2a204d08f2baca36855d88d6f34de20071473314
leonardo-creator <68303803+leonardo-creator@users.noreply.github.com>
2023-12-06 23:26:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageInput = document.getElementById("imageInput");
     const metadataList = document.getElementById("metadataList");
-    const processButton = document.getElementById("processButton");
-    const statusInputs = document.getElementById("statusInputs");
+    const utmCoordinates = document.getElementById("utmCoordinates");
 
     let imageMetadataList = [];
 
-    imageInput.addEventListener("change", function (event) {
+    imageInput.addEventListener("change", async function (event) {
         const files = event.target.files;
 
         if (files.length > 0) {
             clearMetadataList();
             imageMetadataList = [];
-            createStatusInputs(files.length);
 
-            Array.from(files).forEach((file, index) => {
-                readImageMetadata(file, index);
-            });
+            try {
+                await Promise.all(Array.from(files).map((file, index) => readImageMetadata(file, index)));
+                displayMetadataList();
+            } catch (error) {
+                console.error(error);
+            }
         }
-    });
-
-    processButton.addEventListener("click", function () {
-        const jsonMetadata = JSON.stringify(imageMetadataList, null, 2);
-        console.log(jsonMetadata);
-        alert("Verifique o console para ver o JSON gerado.");
     });
 
     function clearMetadataList() {
         metadataList.innerHTML = "";
+        utmCoordinates.innerHTML = "";
     }
 
-    function createStatusInputs(count) {
-        statusInputs.innerHTML = "";
-
-        for (let i = 0; i < count; i++) {
-            const statusGroup = document.createElement("div");
-            statusGroup.innerHTML = `
-                <label for="status-${i}">Status da Foto ${i + 1}:</label>
-                <input type="radio" name="status-${i}" id="status-${i}-pendente" value="Pendente" checked /> Pendente
-                <input type="radio" name="status-${i}" id="status-${i}-concluido" value="Concluído" /> Concluído
-                <input type="radio" name="status-${i}" id="status-${i}-atrasado" value="Atrasado" /> Atrasado
-            `;
-            statusInputs.appendChild(statusGroup);
-        }
+    function displayMetadataList() {
+        imageMetadataList.forEach(metadata => {
+            displayMetadata(metadata);
+        });
     }
 
     function displayMetadata(metadata) {
@@ -52,73 +39,77 @@ document.addEventListener("DOMContentLoaded", function () {
         // Exibir a foto ao lado das propriedades
         const imgElement = document.createElement("img");
         imgElement.src = metadata.thumbnail;
-        imgElement.style.maxWidth = "100px";
-        imgElement.style.maxHeight = "100px";
         listItem.appendChild(imgElement);
 
-        const metadataDiv = document.createElement("div");
-        metadataDiv.style.marginLeft = "10px";
+        const metadataInfo = document.createElement("div");
+        metadataInfo.className = "metadata-info";
 
-        // Exibir as propriedades, o status e o botão de concluir
-        metadataDiv.innerHTML = `<strong>${metadata.name}:</strong>
+        // Exibir as propriedades, o status e a descrição
+        metadataInfo.innerHTML = `<strong>${metadata.name}:</strong>
             <br>
-            <strong>Status:</strong> ${metadata.status}
+            <strong>Status:</strong> 
+            <select id="status-${metadata.index}">
+                <option value="Pendente">Pendente</option>
+                <option value="Concluído">Concluído</option>
+                <option value="Atrasado">Atrasado</option>
+            </select>
             <br>
-            <strong>File Size:</strong> ${metadata["File Size"]}
+            <strong>Descrição:</strong> 
+            <input type="text" id="description-${metadata.index}" placeholder="Descrição" value="${metadata.description || ""}">
             <br>
-            <strong>File Type:</strong> ${metadata["File Type"]}
+            <strong>Data/hora:</strong> ${metadata["Last Modified"]}
             <br>
-            <strong>Last Modified:</strong> ${metadata["Last Modified"]}
-            <br>
-            <strong>Latitude:</strong> ${metadata.Latitude}
-            <br>
-            <strong>Longitude:</strong> ${metadata.Longitude}`;
+            <strong>Coordenadas UTM:</strong> ${calculateUTM(metadata.Latitude, metadata.Longitude)}`;
 
-        const statusButton = document.createElement("button");
-        statusButton.textContent = "Concluir";
-        statusButton.addEventListener("click", function () {
-            // Alterar o status quando o botão é clicado
-            metadata.status = getStatusValue(metadata.index);
-            displayMetadata(metadata);
+        metadataInfo.addEventListener("change", function () {
+            metadata.status = document.getElementById(`status-${metadata.index}`).value;
+            metadata.description = document.getElementById(`description-${metadata.index}`).value;
         });
 
-        metadataDiv.appendChild(statusButton);
-        listItem.appendChild(metadataDiv);
+        listItem.appendChild(metadataInfo);
 
         metadataList.appendChild(listItem);
+
     }
 
     function readImageMetadata(file, index) {
-        const reader = new FileReader();
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
 
-        reader.onload = function (e) {
-            const img = new Image();
-            img.src = e.target.result;
+            reader.onload = function (e) {
+                const img = new Image();
+                img.src = e.target.result;
 
-            img.onload = function () {
-                EXIF.getData(img, function () {
-                    const lat = EXIF.getTag(this, "GPSLatitude");
-                    const lon = EXIF.getTag(this, "GPSLongitude");
+                img.onload = function () {
+                    EXIF.getData(img, function () {
+                        const lat = EXIF.getTag(this, "GPSLatitude");
+                        const lon = EXIF.getTag(this, "GPSLongitude");
 
-                    const metadata = {
-                        index: index,
-                        name: file.name,
-                        status: getStatusValue(index),
-                        "File Size": `${(file.size / 1024).toFixed(2)} KB`,
-                        "File Type": file.type,
-                        "Last Modified": file.lastModifiedDate.toLocaleDateString(),
-                        Latitude: lat ? convertDMSToDD(lat) : "N/A",
-                        Longitude: lon ? convertDMSToDD(lon) : "N/A",
-                        thumbnail: e.target.result,
-                    };
+                        const metadata = {
+                            index: index,
+                            name: file.name,
+                            status: "Pendente",
+                            description: "",
+                            "File Size": `${(file.size / 1024).toFixed(2)} KB`,
+                            "File Type": file.type,
+                            "Last Modified": file.lastModifiedDate.toLocaleDateString(),
+                            Latitude: lat ? convertDMSToDD(lat) : "N/A",
+                            Longitude: lon ? convertDMSToDD(lon) : "N/A",
+                            thumbnail: e.target.result,
+                        };
 
-                    imageMetadataList[index] = metadata;
-                    displayMetadata(metadata);
-                });
+                        imageMetadataList[index] = metadata;
+                        resolve();
+                    });
+                };
             };
-        };
 
-        reader.readAsDataURL(file);
+            reader.onerror = function (error) {
+                reject(error);
+            };
+
+            reader.readAsDataURL(file);
+        });
     }
 
     function convertDMSToDD(coord) {
@@ -130,13 +121,106 @@ document.addEventListener("DOMContentLoaded", function () {
         return dd;
     }
 
-    function getStatusValue(index) {
-        const statusRadios = document.getElementsByName(`status-${index}`);
-        for (const radio of statusRadios) {
-            if (radio.checked) {
-                return radio.value;
-            }
-        }
-        return "Pendente"; // Valor padrão caso nenhum rádio esteja marcado
+    // Função para calcular coordenadas UTM no Brasil
+    function calculateUTM(latitude, longitude) {
+        proj4.defs("WGS84", "+proj=longlat +datum=WGS84 +no_defs");
+
+        // Determinar a zona UTM com base na latitude
+        const zone = 22;
+        const hemisferio = -latitude < 0 ? " +south" : ""; // Adiciona +south se a latitude for negativa
+        const utmDefinition = `+proj=utm +zone=${zone}${hemisferio} +datum=WGS84 +units=m +no_defs`;
+        proj4.defs(`UTM${zone}`, utmDefinition);
+
+        const utm = proj4("WGS84", `UTM${zone}`, [-longitude, -latitude]);
+
+        // Inverter a lógica para longitudes oeste e latitudes sul
+        const utmEasting = utm[0];
+        const utmNorthing = utm[1];
+
+        return `UTM X: ${utmEasting.toFixed(3)}, UTM Y: ${utmNorthing.toFixed(3)}`;
     }
+
+    // Add event listener for the "Concluir" button
+    const concluirButton = document.getElementById("concluirButton");
+    concluirButton.addEventListener("click", function () {
+        concluir();
+    });
+
+    function resizeImage(img, maxWidth, maxHeight) {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        // Set the canvas size to the maximum dimensions
+        canvas.width = maxWidth;
+        canvas.height = maxHeight;
+
+        // Draw the image onto the canvas
+        ctx.drawImage(img, 0, 0, maxWidth, maxHeight);
+
+        // Convert the canvas back to a data URL
+        return canvas.toDataURL('image/jpeg');
+    }
+
+
+    // Function to generate and display JSON
+    function concluir() {
+        // Create a JSON string from the metadataList
+        const jsonContent = JSON.stringify(imageMetadataList, null, 2);
+
+        // Display the JSON content (you can modify this based on your UI)
+        console.log("JSON content:\n\n" + jsonContent);
+
+        // Create a div to hold the content
+        const container = document.createElement('div');
+        container.className = 'document-container'; // Aplica a classe de estilo ao container geral
+
+        // Iterate over each image metadata
+        imageMetadataList.forEach(image => {
+            // Create a row for each image
+            const row = document.createElement('div');
+            row.className = 'row';
+
+            // Create the first column for images
+            const imgColumn = document.createElement('div');
+            imgColumn.className = 'column';
+
+            // Create an image element
+            const imgElement = document.createElement('img');
+            imgElement.src = image.thumbnail;
+            imgElement.className = 'image';
+            imgColumn.appendChild(imgElement);
+
+            // Create the second column for information
+            const infoColumn = document.createElement('div');
+            infoColumn.className = 'column info';
+            const infoParagraph = document.createElement('p');
+            infoParagraph.textContent = `Name: ${image.name}\nStatus: ${image.status}\nFile Size: ${image["File Size"]}\nFile Type: ${image["File Type"]}\nLast Modified: ${image["Last Modified"]}\nLatitude: ${image.Latitude}\nLongitude: ${image.Longitude}`;
+            infoColumn.appendChild(infoParagraph);
+
+            // Append columns to the row
+            row.appendChild(imgColumn);
+            row.appendChild(infoColumn);
+
+            // Append the row to the container
+            container.appendChild(row);
+        });
+
+        // Convert the HTML content to Word format using html-docx-js
+        const content = container.innerHTML;
+        const converted = htmlDocx.asBlob(content);
+
+        // Create a download link
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(converted);
+        link.download = 'output.docx';
+
+        // Trigger the download
+        link.click();
+    }
+
+
+            
+    
 });
+
+
